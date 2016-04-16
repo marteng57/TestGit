da680229617176ca467b64059fcd894beda0d58d
Martin Welti <martinwelti2@martins-mac.local>
2016-04-16 12:06:19 -0700

    Initial Commit

---
+++ Kompass1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kompass1
+//
+//  Created by Martin Welti on 16.04.16.
+//  Copyright © 2016 Martin Welti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
